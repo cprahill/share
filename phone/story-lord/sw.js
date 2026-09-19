9873476dc6c59839d2a35ce403be66a50fec03b0
cprahill <cprahill@gmail.com>
2026-09-19 17:38:57 -0500

    phone: Extra Crossing + Night Heat for eye

--- phone/story-lord/sw.js
+++ phone/story-lord/sw.js
@@ -1,1 +1,1 @@
-self.addEventListener('install',e=>{self.skipWaiting()});
+const C='sl-v4';self.addEventListener('install',e=>{self.skipWaiting()});self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))))});
